72b9f98de013bea1d53d6ad8d0f5048f3a6309c9
EricSchles <ericschles@gmail.com>
2014-09-19 05:31:25 -0700

    clicks map now

--- scraping.py
+++ scraping.py
@@ -14,6 +14,6 @@ elem = driver.find_element_by_id("submit_state")
 elem.click()
 dbElement = WebDriverWait(driver, 20).until(lambda x : x.find_element_by_class_name("state_search_even"))
 
-links = driver.find_element_by_xpath('//div[@class="state_search_even"]')
-
+links = driver.find_element_by_xpath('//div[@class="state_search_even"]//div[@class="branch_name"]')
+links.click()
 driver.close()
