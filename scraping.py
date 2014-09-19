13cd2221c4d58c00f22866584ab9d75f891cf941
EricSchles <ericschles@gmail.com>
2014-09-19 05:55:16 -0700

    error handling added

--- scraping.py
+++ scraping.py
@@ -14,6 +14,20 @@ elem = driver.find_element_by_id("submit_state")
 elem.click()
 dbElement = WebDriverWait(driver, 20).until(lambda x : x.find_element_by_class_name("state_search_even"))
 
-links = driver.find_element_by_xpath('//div[@class="state_search_even"]//div[@class="branch_name"]')
-links.click()
+clicker = driver.find_element_by_xpath('//div[@class="state_search_even"]')
+links = clicker.find_elements_by_xpath('//div[@class="branch_name"]')
+for link in links:
+    #ensure all elements are visited
+    print link.text
+    link.click()
+    #after clicking then look at the map
+    try:
+        WebDriverWait(driver,20).until(lambda x: x.find_element_by_class_name("branch_detail_map"))
+        load_map = link.find_element_by_xpath('//span[@class="branch_detail_map"]')
+        load_map.click()
+        WebDriverWait(driver,20).until(lambda x: x.find_element_by_id("map-container"))
+        desired_map = driver.find_element_by_id("map-container")
+        #this is the map we want, do whatever with the data from here
+    except:
+        continue
 driver.close()
