8175c5e0a340234848a59030bd7877fe8e046076
EricSchles <ericschles@gmail.com>
2014-09-19 05:26:51 -0700

    first commit

---
+++ scraping.py
@@ -0,0 +1,19 @@
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+
+driver = webdriver.Firefox()
+
+driver.get("https://ww2.aauw.org/branch_locator/index.php")
+elem = driver.find_element_by_id("submit_state")
+elem.click()
+dbElement = WebDriverWait(driver, 20).until(lambda x : x.find_element_by_class_name("state_search_even"))
+
+links = driver.find_element_by_xpath('//div[@class="state_search_even"]')
+
+driver.close()
